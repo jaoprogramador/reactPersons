b34f8729cce75e96ad51d0bce3808fd8b1536bab
jao <jaoprogramador@gmail.com>
2024-10-06 11:09:44 +0200

    Error tlf no muestra

--- index.js
+++ index.js
@@ -27,22 +27,22 @@ let persons = [
   { 
     "id": 1,
     "name": "Arto Hellas", 
-    "number": "040-123456"
+    "tlf": "040-123456"
   },
   { 
     "id": 2,
     "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
+    "tlf": "39-44-5323523"
   },
   { 
     "id": 3,
     "name": "Dan Abramov", 
-    "number": "12-43-234345"
+    "tlf": "12-43-234345"
   },
   { 
     "id": 4,
     "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
+    "tlf": "39-23-6423122"
   }
 ];
 
@@ -107,7 +107,7 @@ app.get('/info', (req, res) => {
   
     const person = {
       name: body.name,
-      number: body.number,
+      tlf: body.tlf,
       id: generateId(),
     }
   
